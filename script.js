32b4401f7ddd5418174871f85f657a8ebf192818
XDobson <xander.shaffer@stu.caverna.kyschools.us>
2022-02-16 14:06:31 -0600

    oops forgot 1c

--- script.js
+++ script.js
@@ -23,6 +23,10 @@ if (randomNum == 1) {
     console.log("Greater than or equal to 4");
 }
 
+if (randomNum != 3) {
+    console.log("Not equal to 3");
+}
+
 if (randomNum != 3 && randomNum != 5) {
     console.log("Not equal to 5 or 3");
 }
